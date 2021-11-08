e191694a2ce7c3b444fec7cccc3eaf2fc16593a8
rahul yadav <ry219613@gmail.com>
2021-11-08 18:28:28 +0530

    Added test case should return Happy when mood is anything

---
+++ com/labz/mood/MoodAnalyzerTest.java
@@ -0,0 +1,25 @@
+package com.labz.mood;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.mood.MoodAnalyzer;
+
+public class MoodAnalyzerTest {
+	@Test
+	public void givenMessage_IsProper_ShouldReturnSad() {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+		moodAnalyser.setMessage("I am Sad");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("SAD", actualResult);
+	}
+	
+	@Test
+	public void givenMessage_IsProper_ShouldReturnHappy() {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+		moodAnalyser.setMessage("I am in any mood");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", actualResult);
+	}
+
+}
