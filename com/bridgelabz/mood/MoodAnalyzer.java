384f285b61ddb5d431cf4160272fcbd0bf1a8230
rahul yadav <ry219613@gmail.com>
2021-11-08 18:14:37 +0530

    tested the null to throw MoodAnalysisException and return invalid mood

--- com/bridgelabz/mood/MoodAnalyzer.java
+++ com/bridgelabz/mood/MoodAnalyzer.java
@@ -3,21 +3,52 @@ package com.bridgelabz.mood;
 public class MoodAnalyzer {
 	
 	
-	public String analyseMood(String message) {
-	if (message.toLowerCase().contains("sad")) {
-        return "SAD";
-    } else if (message.toLowerCase().contains("any")) {
-        return "HAPPY";
-    } else {
-        return null;
-    }
-}
+	String message;
 
-public static void main(String[] args) {
-	MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-	String mood = moodAnalyzer.analyseMood("I am happy");
-	System.out.println(mood);
-	mood = moodAnalyzer.analyseMood("I am sad");
-	System.out.println(mood);
+	public MoodAnalyzer() {
+		
+	}
+	
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String analyseMood() throws MoodAnalysisException{
+		try {
+			if (message.toLowerCase().contains("sad")) {
+				return "SAD";
+			}
+			else {
+				return "HAPPY";
+			}
+		}
+		catch (NullPointerException e) {
+			throw new MoodAnalysisException("Entered Invalid Mood");
+		}
+    }
+	
+	public static void main(String[] args) throws MoodAnalysisException  {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+		
+		moodAnalyser.setMessage("I am Happy");
+		System.out.println(moodAnalyser.analyseMood());
+
+		moodAnalyser.setMessage("I am Sad");
+		System.out.println(moodAnalyser.analyseMood());
+		
+		moodAnalyser.setMessage(null);
+		try {
+			System.out.println(moodAnalyser.analyseMood());
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- com/labz/mood/MoodAnalyzerTest.java
+++ com/labz/mood/MoodAnalyzerTest.java
@@ -3,23 +3,37 @@ package com.labz.mood;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.mood.MoodAnalysisException;
 import com.bridgelabz.mood.MoodAnalyzer;
 
 
 public class MoodAnalyzerTest {
 	@Test
-	public void givenMessage_IsProper_ShouldReturnSad() {
+	public void givenMessage_IsProper_ShouldReturnSad() throws MoodAnalysisException {
 		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-		String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
+		moodAnalyser.setMessage("I am Sad");
+		String actualResult = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", actualResult);
 	}
+	
 	@Test
-	public void givenMessage_IsProper_ShouldReturnHappy() {
+	public void givenMessage_IsProper_ShouldReturnHappy() throws MoodAnalysisException {
 		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-		String actualResult = moodAnalyser.analyseMood("I am in Any Mood");
+		moodAnalyser.setMessage("I am in any mood");
+		String actualResult = moodAnalyser.analyseMood();
 		Assert.assertEquals("HAPPY", actualResult);
 	}
 	
+	@Test
+    public void givenMessage_Null_ShouldReturnInvalidMood() throws MoodAnalysisException {
+		MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        moodAnalyser.setMessage(null);
+        try {
+        	String actualResult = moodAnalyser.analyseMood();
+	        Assert.assertEquals("Invalid Mood", actualResult);
+        }
+        catch(MoodAnalysisException e) {
+        	System.out.println("Exception occured :" + e);
+        }
+	}
 }
-
-	
